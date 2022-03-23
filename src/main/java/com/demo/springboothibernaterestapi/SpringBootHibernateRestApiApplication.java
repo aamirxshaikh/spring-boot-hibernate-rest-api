1f8ec150d5cc90de6a77267be72a68e8ea5f80e3
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-03-23 22:49:46 +0530

    first commit

---
+++ src/main/java/com/demo/springboothibernaterestapi/SpringBootHibernateRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springboothibernaterestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHibernateRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/springboothibernaterestapi/SpringBootHibernateRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springboothibernaterestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
