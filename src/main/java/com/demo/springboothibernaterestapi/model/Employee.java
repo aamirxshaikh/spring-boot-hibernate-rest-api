4751dab5ac541e83bc617c95dc4649fd05e85921
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-05-24 20:59:10 +0530

    Testing EmployeeController getAllEmployees and getEmployeeById methods

--- src/main/java/com/demo/springboothibernaterestapi/model/Employee.java
+++ src/main/java/com/demo/springboothibernaterestapi/model/Employee.java
@@ -1,6 +1,8 @@
 package com.demo.springboothibernaterestapi.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "Employees")
 public class Employee {
     @Id
---
+++ src/test/java/com/demo/springboothibernaterestapi/controller/EmployeeControllerTest.java
@@ -0,0 +1,100 @@
+package com.demo.springboothibernaterestapi.controller;
+
+import com.demo.springboothibernaterestapi.model.Employee;
+import com.demo.springboothibernaterestapi.service.EmployeeService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(EmployeeController.class)
+class EmployeeControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @Disabled
+    void saveEmployee() throws Exception {
+    }
+
+    @Test
+    void getAllEmployees() throws Exception {
+        Employee employee1 = new Employee(1L, "John","Doe", "Doe@test.com");
+
+        Employee employee2 = new Employee(2L, "Jane", "Doe", "jane@test.com");
+
+        when(employeeService.getAllEmployees()).thenReturn(Arrays.asList(employee1, employee2));
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", Matchers.is(2)))
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].firstName").value("John"))
+                .andExpect(jsonPath("$[0].lastName").value("Doe"))
+                .andExpect(jsonPath("$[0].email").value("Doe@test.com"))
+                .andExpect(jsonPath("$[1].id").value("2"))
+                .andExpect(jsonPath("$[1].firstName").value("Jane"))
+                .andExpect(jsonPath("$[1].lastName").value("Doe"))
+                .andExpect(jsonPath("$[1].email").value("jane@test.com"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void getEmployeeById() throws Exception {
+        long employeeId = 1L;
+
+        Employee employee = new Employee(employeeId, "John","Doe", "john@test.com");
+
+        when(employeeService.getEmployeeById(employeeId)).thenReturn(employee);
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/employees/{id}", employeeId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", Matchers.is(4)))
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.firstName").value("John"))
+                .andExpect(jsonPath("$.lastName").value("Doe"))
+                .andExpect(jsonPath("$.email").value("john@test.com"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @Disabled
+    void updateEmployee() {
+    }
+
+    @Test
+    @Disabled
+    void deleteEmployee() {
+    }
+}
